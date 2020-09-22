884b1c327741b53017411539f60a543202f4fba4
DEsimas <simasuh@gmail.com>
2020-09-22 13:12:02 +0300

    Доработано изменение размеров окна

--- TryToPress/Form1.Designer.cs
+++ TryToPress/Form1.Designer.cs
@@ -28,25 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label1;
             this.button = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.height = new System.Windows.Forms.TextBox();
+            this.width = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(122, 28);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(20, 20);
+            label1.TabIndex = 9;
+            label1.Text = "X";
             // 
             // button
             // 
             this.button.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button.Location = new System.Drawing.Point(11, 11);
-            this.button.Margin = new System.Windows.Forms.Padding(2);
+            this.button.Location = new System.Drawing.Point(16, 17);
             this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(82, 52);
+            this.button.Size = new System.Drawing.Size(123, 80);
             this.button.TabIndex = 0;
             this.button.TabStop = false;
             this.button.Text = "Нажми на меня";
@@ -56,106 +63,65 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(114, 77);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(148, 57);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 19);
+            this.button1.Size = new System.Drawing.Size(109, 29);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Console";
+            this.button1.Text = "Консоль";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(4, 17);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(41, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Full";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(4, 39);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(42, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "1/2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(4, 61);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(42, 17);
-            this.radioButton3.TabIndex = 4;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "1/4";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Location = new System.Drawing.Point(4, 17);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(103, 89);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Размер скачка";
-            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(label1);
+            this.groupBox2.Controls.Add(this.height);
+            this.groupBox2.Controls.Add(this.width);
             this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.groupBox1);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(11, 11);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Location = new System.Drawing.Point(16, 17);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(200, 121);
+            this.groupBox2.Size = new System.Drawing.Size(272, 105);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Настройки";
             // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(148, 28);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(109, 26);
+            this.height.TabIndex = 8;
+            this.height.Text = "1080";
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(7, 25);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(109, 26);
+            this.width.TabIndex = 7;
+            this.width.Text = "1920";
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(114, 17);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(6, 57);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 19);
+            this.button2.Size = new System.Drawing.Size(110, 29);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Применить";
+            this.button2.Text = "Поехали";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(300, 140);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Try To Press";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -164,12 +130,10 @@
 
         private System.Windows.Forms.Button button;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox height;
+        private System.Windows.Forms.TextBox width;
     }
 }
 
--- TryToPress/Form1.cs
+++ TryToPress/Form1.cs
@@ -14,12 +14,11 @@ namespace TryToPress
     {
         Random rand = new Random();
         Console console = new Console();
-        private int x, y,size_x = 1300,size_y = 700, a = 1;
         private string str = "Console\n";
+        int size_x, size_y, x, y, counter;
         public Form1()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized;
         }
 
         private void button_Click(object sender, EventArgs e)
@@ -35,6 +34,11 @@ namespace TryToPress
         private void button2_Click(object sender, EventArgs e)
         {
             groupBox2.Visible = false;
+            size_x = Int32.Parse(width.Text);
+            size_y = Int32.Parse(height.Text);
+            this.Width = size_x;
+            this.Height = size_y;
+            this.Location = new Point(0, 0);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -42,33 +46,11 @@ namespace TryToPress
             console.Show();
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            size_x = 650;
-            size_y = 350;
-            this.Width = 650;
-            this.Height = 350;
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            size_x = 325;
-            size_y = 175;
-            this.Width = 325;
-            this.Height = 175;
-        }
-
         private void button_MouseEnter(object sender, EventArgs e)
         {
-            x = rand.Next(1,size_x);
-            y = rand.Next(1,size_y);
-            str += a + ".X." + x +" Y." + y + "\n";
-            a += 1;
+            x = rand.Next(1,size_x - button1.Width - 100);
+            y = rand.Next(1,size_y - button1.Height - 100);
+            str += ++counter + ".X." + x +" Y." + y + "\n";
             console.str = this.str;
             console.change();
             button.Location = new Point(x, y);
